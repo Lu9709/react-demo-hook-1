0bd4ba8ed503b1b91024dca5958055d705a7f2d9
lu <919041098@qq.com>
2021-03-27 22:01:03 +0800

    最简单的useState

--- src/index.js
+++ src/index.js
@@ -2,34 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-let _state=[]
-let index = 0
+let _state
 const myUseState = (init) => {
-  const currentIndex = index;
-  _state[currentIndex] = _state[currentIndex] === undefined ? init : _state[currentIndex]
-  console.log("currentIndex")
-  console.log(currentIndex)
+  _state = _state === undefined ? init : _state
   const setState = (newValue) => {
-    _state[currentIndex] = newValue
+    _state = newValue
     render()
   }
-  index += 1;
-  return [_state[currentIndex], setState]
+  return [_state, setState]
 
 }
 const render = () => {
-   index = 0;
   ReactDOM.render(<App/>, document.getElementById('root'))
 }
 function App() {
   const [n, setN] = myUseState(0)
-  const [m, setM] = myUseState(0)
   return (
     <div>
       {n}
       <button onClick={() => setN(n + 1)}>+1</button>
-      {m}
-      <button onClick={() => setM(m + 1)}>+1</button>
     </div>
   )
 }
--- src/useUpdate.js
+++ src/useUpdate.js
@@ -1,14 +1,14 @@
-import {useEffect, useState} from "react"
-
-const useUpdate = (fn, dep) => {
-  const [count, setCount] = useState(0)
-  useEffect(() => {
-    setCount(x => x + 1)
-  }, [dep])
-  useEffect(() => {
-    if (count > 1) {
-      fn()
-    }
-  }, [count, fn])
-}
-export default useUpdate
+// import {useEffect, useState} from "react"
+//
+// const useUpdate = (fn, dep) => {
+//   const [count, setCount] = useState(0)
+//   useEffect(() => {
+//     setCount(x => x + 1)
+//   }, [dep])
+//   useEffect(() => {
+//     if (count > 1) {
+//       fn()
+//     }
+//   }, [count, fn])
+// }
+// export default useUpdate
