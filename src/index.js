a9c19e87cd8be3541b8b96b360feca3cbb5c8dca
lu <919041098@qq.com>
2021-03-27 20:48:15 +0800

    如何让两个useState不冲突

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
 
+let _state=[]
+let index = 0
+const myUseState = (init) => {
+  const currentIndex = index;
+  _state[currentIndex] = _state[currentIndex] === undefined ? init : _state[currentIndex]
+  console.log("currentIndex")
+  console.log(currentIndex)
+  const setState = (newValue) => {
+    _state[currentIndex] = newValue
+    render()
+  }
+  index += 1;
+  return [_state[currentIndex], setState]
 
+}
+const render = () => {
+   index = 0;
+  ReactDOM.render(<App/>, document.getElementById('root'))
+}
+function App() {
+  const [n, setN] = myUseState(0)
+  const [m, setM] = myUseState(0)
+  return (
+    <div>
+      {n}
+      <button onClick={() => setN(n + 1)}>+1</button>
+      {m}
+      <button onClick={() => setM(m + 1)}>+1</button>
+    </div>
+  )
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <App/>
+  </>,
   document.getElementById('root')
 );
