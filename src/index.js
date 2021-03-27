595c444a40864d5f59a1e74f91c0d1c3da2b426a
lu <919041098@qq.com>
2021-03-27 22:38:53 +0800

    useRef 强制跟新

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 
 function App() {
   const nRef = React.useRef(0)
+  const update = React.useState()[1]
   return (
     <div>
       <p>{nRef.current} 这里不能实时跟新</p>
-      <button onClick={() => (nRef.current += 1)}>+1</button>
+      <button onClick={() => ((nRef.current += 1),update(nRef.current))}>+1</button>
       {console.log(`${nRef.current}`)}
       <button onClick={()=>console.log(`n:${nRef.current}`)}>log</button>
     </div>
