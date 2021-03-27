34dab86a5a959e072320104f757d174de714afad
lu <919041098@qq.com>
2021-03-27 23:02:54 +0800

    useContext

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const ThemeContext = React.createContext(null);
+
 function App() {
-  const nRef = React.useRef(0)
-  const update = React.useState()[1]
+  const [n, setN] = React.useState(0)
   return (
     <div>
-      <p>{nRef.current} 这里不能实时跟新</p>
-      <button onClick={() => ((nRef.current += 1),update(nRef.current))}>+1</button>
-      {console.log(`${nRef.current}`)}
-      <button onClick={()=>console.log(`n:${nRef.current}`)}>log</button>
+      <ThemeContext.Provider value={{n,setN}}>
+        <p>{n} </p>
+        <button onClick={() => setN(n + 1)}>+1</button>
+      </ThemeContext.Provider>
     </div>
   )
 }
