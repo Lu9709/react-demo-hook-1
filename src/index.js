ffa4c18d679b48f6a4fee25731309268839cfb63
lu <919041098@qq.com>
2021-03-28 15:01:03 +0800

    useCallback

--- src/index.js
+++ src/index.js
@@ -1,29 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {useCallback,useMemo} from 'react'
 
 function App() {
   const [n, setN] = React.useState(0);
   const [m, setM] = React.useState(0);
+
   const onClick = () => {
     setN(n + 1);
   };
+  const onClick2 = () => {
+    setM(m + 1);
+  };
+  const onClickChild = useCallback( ()=>{console.log(m)}
+  ,[m])
 
   return (
     <div className="App">
       <div>
         <button onClick={onClick}>update n {n}</button>
+        <button onClick={onClick2}>update m {m}</button>
       </div>
-      <Child2 data={m}/>
-      {/* <Child2 data={m}/> */}
+      <Child2 data={m} onClick={onClickChild} />
+      {/* Child2 居然又执行了 */}
     </div>
   );
 }
 
 function Child(props) {
   console.log("child 执行了");
-  console.log('假设这里有大量代码')
-  return <div>child: {props.data}</div>;
+  console.log("假设这里有大量代码");
+  return <div onClick={props.onClick}>child: {props.data}</div>;
 }
 
 const Child2 = React.memo(Child);
