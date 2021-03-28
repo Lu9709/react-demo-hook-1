a9cc4baee910b804461e16c550e8cc47376acfd7
lu <919041098@qq.com>
2021-03-28 15:28:37 +0800

    useRef

--- src/index.js
+++ src/index.js
@@ -1,39 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {useCallback,useMemo} from 'react'
+
 
 function App() {
   const [n, setN] = React.useState(0);
-  const [m, setM] = React.useState(0);
-
+  const count = React.useRef(0)
   const onClick = () => {
     setN(n + 1);
   };
-  const onClick2 = () => {
-    setM(m + 1);
-  };
-  const onClickChild = useCallback( ()=>{console.log(m)}
-  ,[m])
-
+  React.useEffect(
+    ()=>{
+      count.current +=1
+      console.log(count.current)
+    }
+  )
   return (
     <div className="App">
-      <div>
         <button onClick={onClick}>update n {n}</button>
-        <button onClick={onClick2}>update m {m}</button>
-      </div>
-      <Child2 data={m} onClick={onClickChild} />
-      {/* Child2 居然又执行了 */}
     </div>
   );
 }
-
-function Child(props) {
-  console.log("child 执行了");
-  console.log("假设这里有大量代码");
-  return <div onClick={props.onClick}>child: {props.data}</div>;
-}
-
-const Child2 = React.memo(Child);
 ReactDOM.render(
   <>
     <App/>
