359508c03d4815f126cec1147d64fb55a4479545
lu <919041098@qq.com>
2021-03-28 18:08:59 +0800

    useImperativeHandle

--- src/index.js
+++ src/index.js
@@ -1,24 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {createRef,useEffect,useRef,useImperativeHandle} from  'react'
 
 function App() {
-  const buttonRef = React.useRef(null);
+  const buttonRef = useRef(null);
+  useEffect(() => {
+    console.log(buttonRef.current);
+  });
   return (
     <div className="App">
-      <Button ref={buttonRef}>按钮</Button>
+      <Button2 ref={buttonRef}>按钮</Button2>
+      <button
+        className="close"
+        onClick={() => {
+          console.log(buttonRef);
+          buttonRef.current.x();
+        }}
+      >
+        x
+      </button>
     </div>
   );
 }
-// const Button3 = React.forwardRef((props, ref) => {
-//   console.log(props);
-//   console.log(ref)
-//   return <button className="red" ref={ref} {...props} />;
-//
-// });
-const Button = React.forwardRef((props,ref)=>{
-  return <button className='red' ref={ref} {...props}/>
-})
+
+const Button2 = React.forwardRef((props, ref) => {
+  const realButton = createRef(null);
+  const setRef = useImperativeHandle;
+  setRef(ref, () => {
+    return {
+      x: () => {
+        realButton.current.remove();
+      },
+      realButton: realButton
+    };
+  });
+  return <button ref={realButton} {...props} />;
+});
 ReactDOM.render(
   <>
     <App/>
